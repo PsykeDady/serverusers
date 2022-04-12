444d65f780de353c37d3192c86d9a3f2e799fe1a
PsykeDady <psdady@msn.com>
2022-04-12 14:59:37 +0200

    started work for reactive forms

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -10,37 +10,7 @@ import { UserService } from "src/app/services/User.service";
 	templateUrl:"register.component.html",
 	styleUrls:["register.component.css"]
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-	@ViewChild("formRegistrazione") formRegistrazione:NgForm;
-
-	constructor(public router:Router, public userService: UserService, public serverService: ServerService){}
-
-	ngOnInit(): void {
-		console.log(this.formRegistrazione)
-	}
-
-	stampe(){
-		console.log("NgForm=",this.formRegistrazione);
-		console.log("NgForm.valid=",this.formRegistrazione.valid);
-		console.log("NgForm.value=",this.formRegistrazione.value);
-		console.log("NgForm.value[\"nome\"]=",this.formRegistrazione.value["nome"]);
-	}
-
-	registrati(){
-		this.stampe()
-		let tipoAggiunta=this.formRegistrazione.value["tipoAggiunta"]; 
-
-		if (tipoAggiunta=="user"){
-			this.userService.addUsers(this.formRegistrazione.value["nome"])
-
-			this.router.navigate(["/users"])
-		} else {
-			this.serverService.push(new ServerModel(this.formRegistrazione.value["nome"],false))
-
-
-			this.router.navigate(["/servers"])
-		}
-		
-	}
+	
 }
