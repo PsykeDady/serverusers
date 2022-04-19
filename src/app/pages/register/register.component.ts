4f8f7401190c454db945760100cabd6968fa980c
PsykeDady <psdady@msn.com>
2022-04-19 17:32:45 +0200

    modifiche

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
-import { FormControl, FormGroup, NgForm, NgModelGroup, Validators } from "@angular/forms";
+import { FormArray, FormControl, FormGroup, NgForm, NgModelGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { ServerModel } from "src/app/models/ServerModel";
 import { ServerService } from "src/app/services/Servers.service";
@@ -21,17 +21,20 @@ export class RegisterComponent implements OnInit {
 		this.nomeForm = new FormGroup({
 			"anagrafica": new FormGroup({
 				'nome':new FormControl(null, Validators.required),
-				'email':new FormControl(null, [Validators.required, Validators.email]),
+				'email':new FormControl(null, this.validaEmailSerio),
 			}),
-			'tipo':new FormControl(null, Validators.required)
+			'tipo':new FormControl(null, Validators.required),
+			'listaSkill' : new FormArray([])
 		})
 	}
 
-	submitta(){
-		console.log(this.nomeForm.value["anagrafica"]["nome"])
-		console.log(this.nomeForm.get(['anagrafica','nome']))
-		console.log(this.nomeForm.get('tipo'))
+	submitta():void{
 		console.log(this.nomeForm.value)
+
+		if(this.nomeForm.invalid){
+			alert("Wewe kki sta faciendu")
+			return;
+		}
 
 		if(this.nomeForm.value['tipo']=="server"){
 			this.serverService.push(new ServerModel(this.nomeForm.value["anagrafica"]["nome"],false))
@@ -42,4 +45,30 @@ export class RegisterComponent implements OnInit {
 		}
 	}
 
+	addSkill():void{
+		console.log("this.nomeForm.get('listaSkill').valid",this.nomeForm.get("listaSkill").valid);
+		console.log("this.nomeForm.get('listaSkill').value",this.nomeForm.get("listaSkill").value);
+		console.log("(this.nomeForm.get('listaSkill') as FormArray).controls",(this.nomeForm.get("listaSkill") as FormArray).controls);
+		if(this.nomeForm.get("listaSkill").invalid){
+			return;
+		}
+		(this.nomeForm.get("listaSkill") as FormArray).push(new FormControl(null,Validators.required));
+		console.log("this.nomeForm.get('listaSkill').value fine",this.nomeForm.get("listaSkill").value);
+	}
+
+	getFormArray(){
+		return this.nomeForm.get("listaSkill") as FormArray;
+	}
+
+	validaEmailSerio(formEmail:FormControl):{[s:string]:boolean}{
+
+		let testo:string=formEmail.value;
+
+		let verifica=  testo? testo.match(/[a-zA-Z0-9][a-zA-Z0-9_]*@([a-zA-Z0-9][a-zA-Z0-9_]*\.)+(com|it|info|pizza|org)/g) : null;
+
+		console.log("verifica=",verifica)
+		console.log("formEmail=",formEmail)
+
+		return verifica?null:{'l\'email non è valida':true, 'altromessaggio':false};
+	}
 }
